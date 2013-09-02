d23d3573fa93298090b235f816eaf27e0ca598cb
Darcy Murphy <mrDarcyMurphy@gmail.com>
2013-09-02 13:48:55 -0500

    first test

---
+++ test/the-thing-is.js
@@ -0,0 +1,8 @@
+var assert = require('assert')
+var the = require('../the-thing-is')
+
+describe('the', function() {
+  it('a function', function() {
+    assert(typeof the === 'function')
+  })
+})
