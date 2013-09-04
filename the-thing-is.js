2406c7b5093062649c849cc3da6ecc25f123a3bc
Darcy Murphy <mrDarcyMurphy@gmail.com>
2013-09-04 13:38:45 -0500

    cleaned up notes

--- the-thing-is.js
+++ the-thing-is.js
@@ -1,50 +1,19 @@
 // The Thing Is
 // Configuration driven validation
-// with the goal of a near-readable-english syntax
+// with the goal of near-english syntax
 
 // Usage:
 //
-// var whatYouExpect = 'number' // string (least useful)
-// var whatYouExpect = ['present', 'number', {greaterThan:0}] // array (tightest to write)
-// var whatYouExpect = {present:true, number:true, greaterThan:0} // object (verbose)
+// var whatYouExpect = 'number'  --  string (least useful)
+// var whatYouExpect = ['present', 'number', {greaterThan:0}] -- array (tightest to write)
+// var whatYouExpect = {present:true, number:true, greaterThan:0} -- object (verbose, unreliable order)
 //
-// call a callback when done
-// the(thing).isnt(whatYouExpect).then(function(){ throw Error('NOOOOOOOOOO!') })
-// the(thing).isnt(whatYouExpect).then(throwSomething)
-//
-// optionally chain callback logic
-// the(thing).isnt(whatYouExpect).do(throwSomething).else(drinkUp)
-// the(thing).is(whatYouExpect).do(someThing).else(doSomethingElse)
-//
-// tell the function what to return to avoid the primitive v. object challenge
-// return, do, then could be alias' of each other
-// they'd check typeof argument and run the function, or return the value, or return true/false based on the whether or not the check failed
-// ***********************************
-// if (the(thing).isnt(whatYouExpect)) // automatically returns false because you passed in an array or object
-// ***********************************
-// if (the(thing).isnt(whatYouExpect).return(false))
-// if (the(thing).isnt(whatYouExpect).do(false))
-// if (the(thing).isnt(whatYouExpect).then(false))
-// if (the(thing).isnt(whatYouExpect).then(callback)) // doesn't make sense in an if check
-// if (the(thing).isnt(whatYouExpect).then())
-// if (the(thing).is(whatYouExpect).then() )
-// if (the(thing).is.present().number().gt(5).then())
-// the(thing).isnt(whatYouExpect).then(callback)
-
-// Don't do this
-// it will always be true because it returns an object
-//
-// if (the(thing).is.present().number())
-
-// potential alias
-// ifThe(thing).is.present().number().greaterThan(5).do(callback)
-
-
-// if (the(thing).is(['number', {greaterThan:0}])) {
-//   // celebrate
+// if (the(thing).is(whatYouExpect)) {
+//   fuckYeah()
 // }
-// if (the(thing).isnt(['number', {greaterThan:0}])) {
-//   throw the.last.error
+//
+// if (the(thing).isnt(whatYouExpect)) {
+//   throwSomething()
 // }
 
 // Dependencies
